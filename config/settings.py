7ea13a668211d1ef817031667e0489ab4d9a5fc8
BenitoSwaggolini <profie_player@mail.ru>
2022-11-30 16:59:23 +0500

    Added dotenv

--- config/settings.py
+++ config/settings.py
@@ -9,17 +9,16 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+from dotenv import load_dotenv
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+load_dotenv()
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-faa(b4e3+$()pysath#z*mw*b%^e(yty22^67anwbl1s9obv8c'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -117,6 +116,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-DOMAIN = "http://127.0.0.1:8000"
-STRIPE_PUBLISHABLE_KEY = 'pk_test_51LhZx4Hs1Ric6ZgWX3zjoVkdGnk4KuVRwXENGxukXbABDzsjwNWxsImAelnhQkqiC8v2piyGatTNw7P4LKl1aHzt00FdiHt8bu'
-STRIPE_SECRET_KEY = 'sk_test_51LhZx4Hs1Ric6ZgWI9srsyhXZDBdD0syCfgC1ipQijrAIvjtHoP6ikPCMQDuF324PljUhHLAEdQ904yctoYHkH3Z00xatsnmnh'
+DOMAIN = os.environ.get("DOMAIN")
+STRIPE_PUBLISHABLE_KEY = os.environ.get("STRIPE_PUBLISHABLE_KEY")
+STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
